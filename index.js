53b4307c648ed66c129c6f02ef8efe7ffb1855b4
Brian Salazar <briansalazar5@gmail.com>
2026-04-07 23:27:27 -0700

    Render test data on index.ejs

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ dotenv.config();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-data = [
+var data = [
     {
         id: 1,
         title: "The war of the worlds",
-        cover: "https://covers.openlibrary.org/b/olid/OL33225306M-M.jpg",
+        olid: "OL33225306M",
+        cover: `https://covers.openlibrary.org/b/olid/OL33225306M-M.jpg`,
         score: "10",
         date: "1/20/2025",
         summary: "Lorem ipsum dolor sit amet. Ut minus unde et nisi consequatur ex provident aliquam eos numquam quisquam nam enim similique in placeat natus? Est quae voluptatem qui enim optio quo maiores eligendi non eaque voluptatem aut dolore alias et iusto dolores. Aut quis quod a aliquid libero ut deserunt eaque et aliquam accusantium."
@@ -23,6 +24,7 @@ data = [
     {
         id: 2,
         title: "Barbarians at the Gate",
+        olid: "OL24272655M",
         cover: "https://covers.openlibrary.org/b/olid/OL24272655M-M.jpg",
         score: "9",
         date: "12/30/2025",
@@ -31,6 +33,7 @@ data = [
     {
         id: 3,
         title: "Julius Ceasar",
+        olid: "OL8201309M",
         cover: "https://covers.openlibrary.org/b/olid/OL8201309M-M.jpg",
         score: "9",
         date: "3/15/2026",
@@ -39,7 +42,7 @@ data = [
 ];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {data: data});
 });
 
 app.get("/add", (req, res) => {
